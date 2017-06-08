c189f12ed5226f56f0d5135349367b07e62113c9
Clay Smith <smithclay@gmail.com>
2017-06-08 16:24:32 -0700

    add chrome driver support

--- fetch-dependencies.sh
+++ fetch-dependencies.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
 npm install
-mkdir -p bin && curl -SL https://github.com/adieuadieu/serverless-chrome/blob/master/chrome/chrome-headless-lambda-linux-x64.tar.gz\?raw\=true | tar xJv -C bin --strip-components 1
+mkdir -p bin
+
+curl -SL https://github.com/adieuadieu/serverless-chrome/blob/master/chrome/chrome-headless-lambda-linux-x64.tar.gz\?raw\=true | tar xJv -C bin --strip-components 1
+
+curl -SL https://chromedriver.storage.googleapis.com/2.30/chromedriver_linux64.zip > -d chromedriver.zip
+unzip chromedriver.zip bin/
+rm chromedriver.zip
+
